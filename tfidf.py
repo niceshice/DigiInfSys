7a74d9758202fa8e89d9adf16e95613b3314720f
niceshice <ju.scheuchenpflug@gmail.com>
2021-06-09 11:10:26 +0200

    redid tfidf.py, inv index is no more

--- tfidf.py
+++ tfidf.py
@@ -4,51 +4,31 @@ import re
 import glob
 from tqdm import tqdm
 
-# HA: struktur wie besprochen, also inv index, dict mit invindex und tf pro wort, doclist
-
 corpath = "./erzaehltexte/"
 doclist = glob.glob(f"{corpath}/*.txt")
 invindex = defaultdict(list)
-tfdict = defaultdict(list)
 idfdict = dict()
-helpcounter = defaultdict(Counter)
+helpcounter = Counter()
 
-
-def get_max(docindex):
-    return helpcounter[doclist[docindex]].most_common(1)[0][1]
-
-
-def get_tf(doc, term):
-    return float(helpcounter[doclist[doc]][term] / get_max(doc))     # calculate tf
-
-
-def _main():
-    # make inv index
-    for doc in tqdm(doclist):
+def __main__():
+    # make inv index with tf
+    for docid, doc in tqdm(enumerate(doclist)):
         with open(doc, "r", encoding="utf8") as f:          # iterate all docs in directory
-            docterms = re.findall("\w+", f.read().lower())
+            docterms = re.findall(r"\w+", f.read().lower())
             docset = set(docterms)                          # make set of types per document, with lowered strings
-            helpcounter[doc] = Counter(docterms)            # make Counter object of every doc for later use
-        for types in docset:
-            invindex[types].append(doclist.index(doc))      # append index of doc for each type in current doc
+            helpcounter = Counter(docterms)                 # make Counter object of every doc (with ID as key) for later use
+        for type in docset:
+            invindex[type].append((docid, float(helpcounter[type]/max(helpcounter.values()))))    # append index of doc and tf of type for each type in current doc
 
     with open("./data/invindex.txt", "w", encoding="utf8") as f:
         f.write(str(dict(invindex)))
-
-    # make inv index with tf values
-    for term in tqdm(invindex):
-        for i, doc in tqdm(enumerate(invindex[term])):
-            invindex[term][i] = (doc, get_tf(doc, term))
 
     # make idf dict
     for term in tqdm(invindex):
         idfdict[term] = float(len(invindex[term])/len(doclist))
 
-    with open("./data/tfinvindex.txt", "w", encoding="utf8") as f:
-        f.write(str(dict(invindex)))
-
     with open("./data/idf.txt", "w", encoding="utf8") as f:
         f.write(str(dict(idfdict)))
 
-_main()
+__main__()
 
