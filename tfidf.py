b854bf85fb9e1935ff8ffdba31960734e1f2bac1
niceshice <ju.scheuchenpflug@gmail.com>
2023-06-01 12:09:17 +0200

    update
    updated commit

--- tfidf.py
+++ tfidf.py
@@ -1,14 +1,15 @@
-import math
 from collections import defaultdict, Counter
 import re
 import glob
 from tqdm import tqdm
+import random
 
 corpath = "./erzaehltexte/"
 doclist = glob.glob(f"{corpath}/*.txt")
 invindex = defaultdict(list)
 idfdict = dict()
 helpcounter = Counter()
+outcounter = dict()
 
 def __main__():
     # make inv index with tf
@@ -17,6 +18,7 @@ def __main__():
             docterms = re.findall(r"\w+", f.read().lower())
             docset = set(docterms)                          # make set of types per document, with lowered strings
             helpcounter = Counter(docterms)                 # make Counter object of every doc (with ID as key) for later use
+            outcounter[doc] = helpcounter
         for type in docset:
             invindex[type].append((docid, float(helpcounter[type]/max(helpcounter.values()))))    # append index of doc and tf of type for each type in current doc
 
@@ -26,6 +28,12 @@ def __main__():
     # make idf dict
     for term in tqdm(invindex):
         idfdict[term] = float(len(invindex[term])/len(doclist))
+
+    # make list of random searches
+    ran_list = sorted(invindex.keys())
+    for i in range(10):
+        ran_int = random.randint(0, len(ran_list) - 1)
+        print(ran_list[ran_int:ran_int+20])
 
     with open("./data/idf.txt", "w", encoding="utf8") as f:
         f.write(str(dict(idfdict)))
